92b4a7256d262f2ebec95e66903e3492933b3dcb
BugHunter-X <askzenai@gmail.com>
2024-12-29 19:46:06 +0100

    Add bugSolution.lua file

---
+++ bugSolution.lua
@@ -0,0 +1,17 @@
+local function foo(t)
+  local keys = {}
+  for k in pairs(t) do
+    table.insert(keys, k)
+  end
+  table.sort(keys)
+
+  for _, k in ipairs(keys) do
+    if type(t[k]) == "table" then
+      foo(t[k])
+    end
+  end
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+foo(t)
+print(t.b.d.e)
